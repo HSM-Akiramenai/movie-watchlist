d3ba27f027f30f99a75928b3f3049ff58328aab5
HSM-Akiramenai <115085023+HSM-Akiramenai@users.noreply.github.com>
2024-08-30 18:23:51 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,90 +1,97 @@
-const searchBtn = document.getElementById('search-btn')
-const searchInput = document.getElementById('search-input')
-const omdb_key = process.env.OMDB_API_KEY
-let moviesWithFullData = []
+const searchBtn = document.getElementById("search-btn");
+const searchInput = document.getElementById("search-input");
+let moviesWithFullData = [];
 
-searchBtn.addEventListener('click', handleSearchClick)
+searchBtn.addEventListener("click", handleSearchClick);
 
-async function handleSearchClick(){
-    const res = await fetch(`https://www.omdbapi.com/?apikey=${omdb_key}&s=${searchInput.value}&type=movie`)
-    const data = await res.json()
-    if(data.Response === 'False'){
-        searchInput.value = 'Searching something with no data'
-        document.querySelector('main').innerHTML = `
+async function handleSearchClick() {
+  const res = await fetch(
+    `https://www.omdbapi.com/?apikey=db57fda8&s=${searchInput.value}&type=movie`
+  );
+  const data = await res.json();
+  if (data.Response === "False") {
+    searchInput.value = "Searching something with no data";
+    document.querySelector("main").innerHTML = `
             <div class="no-movies-found-div">
                 <p>Unable to find what you’re looking for. Please try another search.</p>
             </div>
-        `  
-    }else{
-        const movies = data.Search
-        moviesWithFullData = await getMoviesWithFullData(movies)  
-        renderMovies(moviesWithFullData)
-        removeTrailingDivider()
-        addMovieToWatchList()        
-    }  
+        `;
+  } else {
+    const movies = data.Search;
+    moviesWithFullData = await getMoviesWithFullData(movies);
+    renderMovies(moviesWithFullData);
+    removeTrailingDivider();
+    addMovieToWatchList();
+  }
 }
 
-function addMovieToWatchList(){
-    document.addEventListener('click', (e) => {
-        const movieId = e.target.dataset.addMovieId
-        
-        if(movieId){
-            if (localStorage.getItem(`movie-${movieId}`)) {
-                alert('Movie already in your Watchlist.')
-            }else{
-                const movie = moviesWithFullData.find(m => m.id == movieId)  
-                localStorage.setItem(`movie-${movieId}`, JSON.stringify(movie))
-                document.getElementById(`${movieId}`).innerHTML = `<i class="fa-solid fa-circle-check" data-add-movie-id="${movieId}"></i> Watchlist`          
-            }
-        }
-    })
+function addMovieToWatchList() {
+  document.addEventListener("click", (e) => {
+    const movieId = e.target.dataset.addMovieId;
+
+    if (movieId) {
+      if (localStorage.getItem(`movie-${movieId}`)) {
+        alert("Movie already in your Watchlist.");
+      } else {
+        const movie = moviesWithFullData.find((m) => m.id == movieId);
+        localStorage.setItem(`movie-${movieId}`, JSON.stringify(movie));
+        document.getElementById(
+          `${movieId}`
+        ).innerHTML = `<i class="fa-solid fa-circle-check" data-add-movie-id="${movieId}"></i> Watchlist`;
+      }
+    }
+  });
 }
 
 async function getMoviesWithFullData(movies) {
-    const moviePromises = movies.map(async (movie) => {
-        const res = await fetch(`https://www.omdbapi.com/?apikey=${omdb_key}&i=${movie.imdbID}`);
-        const data = await res.json();
-        const { Title, Runtime, Genre, Plot, Poster, imdbID, imdbRating } = data;
-        return {
-            id: imdbID,
-            title: Title,
-            duration: Runtime,
-            genre: Genre,
-            plot: Plot,
-            poster: Poster,
-            rating: imdbRating
-        };
-    });
+  const moviePromises = movies.map(async (movie) => {
+    const res = await fetch(
+      `https://www.omdbapi.com/?apikey=db57fda8&i=${movie.imdbID}`
+    );
+    const data = await res.json();
+    const { Title, Runtime, Genre, Plot, Poster, imdbID, imdbRating } = data;
+    return {
+      id: imdbID,
+      title: Title,
+      duration: Runtime,
+      genre: Genre,
+      plot: Plot,
+      poster: Poster,
+      rating: imdbRating,
+    };
+  });
 
-    return Promise.all(moviePromises);
+  return Promise.all(moviePromises);
 }
 
-function renderMovies(movies){
-    document.querySelector('main').innerHTML = `
+function renderMovies(movies) {
+  document.querySelector("main").innerHTML = `
         <div id="movies" class="movies content-wrapper">
         </div>
-    `
-    
-    document.getElementById('movies').innerHTML = movies.map((movie) => {
-        const {
-            id,
-            title,
-            duration,
-            genre,
-            plot,
-            rating,
-            poster: originalPoster
-        } = movie;
-        
-        const poster = originalPoster === 'N/A'
-            ? '/images/poster-placeholder.png'
-            : originalPoster;
-            
-        const buttonHtml = localStorage.getItem(`movie-${id}`) 
-            ? `<i class="fa-solid fa-circle-check" data-add-movie-id="${id}"></i>Watchlist`
-            : `<i class="fa-solid fa-circle-plus" data-add-movie-id="${id}"></i>Watchlist`
-        
-        return `
+    `;
+
+  document.getElementById("movies").innerHTML = movies
+    .map((movie) => {
+      const {
+        id,
+        title,
+        duration,
+        genre,
+        plot,
+        rating,
+        poster: originalPoster,
+      } = movie;
+
+      const poster =
+        originalPoster === "N/A"
+          ? "/images/poster-placeholder.png"
+          : originalPoster;
+
+      const buttonHtml = localStorage.getItem(`movie-${id}`)
+        ? `<i class="fa-solid fa-circle-check" data-add-movie-id="${id}"></i>Watchlist`
+        : `<i class="fa-solid fa-circle-plus" data-add-movie-id="${id}"></i>Watchlist`;
+
+      return `
             <div class="movie">
                 <img class="poster" src=${poster}>
                 <div class="movie-header">
@@ -101,17 +108,17 @@ function renderMovies(movies){
                 </div>
             </div>
             <div class="movie-divider"></div>
-        `
-    }).join('')
+        `;
+    })
+    .join("");
 }
 
+function removeTrailingDivider() {
+  const dividers = document.querySelectorAll(".movie-divider");
+  const movies = document.getElementById("movies");
 
-function removeTrailingDivider(){
-    const dividers = document.querySelectorAll('.movie-divider')
-    const movies = document.getElementById('movies')
-    
-    if (dividers.length > 0){
-        dividers[dividers.length - 1].style.display = 'none'
-        movies.style.marginBottom = '3.75em'
-    }
+  if (dividers.length > 0) {
+    dividers[dividers.length - 1].style.display = "none";
+    movies.style.marginBottom = "3.75em";
+  }
 }
